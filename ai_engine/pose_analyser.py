5364b0163900420a45c6bb9373cc09d689a4b89d
Shreyas Jadhav <shreyas.jadhav@pricelabs.co>
2025-06-24 20:38:44 +0530

    project setup

---
+++ ai_engine/pose_analyser.py
@@ -0,0 +1,12 @@
+import cv2
+import mediapipe as mp
+
+class PoseAnalyzer:
+    def __init__(self):
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose()
+
+    def analyze(self, frame):
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.pose.process(image_rgb)
+        return results.pose_landmarks
